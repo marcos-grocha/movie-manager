2d172dc55b1df99b5485832b70565916a4c59942
Marcos Guimarães <102266797+marcos-grocha@users.noreply.github.com>
2024-09-25 21:00:44 -0300

    Adicionando endpoints get e post.

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,23 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+
+    if @genre.save
+      redirect_to genres_path, notice: "Genero criado com sucesso"
+    else
+      render :new
+    end
+  end
 end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to @movie, notice: "Filme cadastrado com sucesso"
+    else
+      render :new
+    end
+  end
 end
