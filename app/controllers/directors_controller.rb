ebbdea14f1c41e8d0a75035020c47653155a9eee
Marcos Guimaraes <jr_academicos@hotmail.com>
2024-09-25 21:00:44 -0300

    Adicionando endpoints get e post.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,2 +1,23 @@
 class DirectorsController < ApplicationController
+  def index
+    @directors = Director.all
+  end
+
+  def show
+    @director = Director.find(params[:id])
+  end
+
+  def new
+    @director = Director.new
+  end
+
+  def create
+    @director = Director.new(director_params)
+
+    if @director.save
+      redirect_to @director, notice: "Diretor cadastrado com sucesso!"
+    else
+      render :new
+    end
+  end
 end
